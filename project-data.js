0472b8f342564b37eed4a300f32471f7fc417a39
nithisith-source <nithisith@gmail.com>
2026-09-17 13:28:48 +0700

    Edit Project 47: K'Dear Residence

--- project-data.js
+++ project-data.js
@@ -3171,13 +3171,13 @@ next: "05"
     country:
       "Thailand",
 
-    year: "",
+    year: "2020",
 
-    completion: "",
+    completion: "2021",
 
-    area: "",
+    area: "280",
 
-    areaNumber: 0,
+    areaNumber: 280,
 
     status:
       "Draft",
